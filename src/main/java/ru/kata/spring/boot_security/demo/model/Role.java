333771a4ca10033d62414e5419dbd58a37528d5d
Sergey <iamrussia@mail.ru>
2025-06-25 15:47:23 +0700

    Подключен lombok в User и Role

--- src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -1,8 +1,11 @@
 package ru.kata.spring.boot_security.demo.model;
 
-import org.springframework.security.core.GrantedAuthority;
+import lombok.Data;
+//import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
@@ -10,43 +13,42 @@ import javax.persistence.Transient;
 import java.util.Set;
 
 @Entity
-@Table(name = "role")
-public class Role implements GrantedAuthority {
+@Data
+@Table(name = "roles")
+public class Role {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
     @Transient
     @ManyToMany(mappedBy = "roles")
     private Set<User> users;
     public Role() {
     }
-    public Role(Long id) {
-        this.id = id;
-    }
-    public Role(Long id, String name) {
-        this.id = id;
+    public Role(String name) {
         this.name = name;
     }
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public Set<User> getUsers() {
-        return users;
-    }
-    public void setUsers(Set<User> users) {
-        this.users = users;
-    }
-    @Override
-    public String getAuthority() {
-        return getName();
-    }
+//    public Long getId() {
+//        return id;
+//    }
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//    public String getName() {
+//        return name;
+//    }
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//    public Set<User> getUsers() {
+//        return users;
+//    }
+//    public void setUsers(Set<User> users) {
+//        this.users = users;
+//    }
+//    @Override
+//    public String getAuthority() {
+//        return getName();
+//    }
 }
--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -1,9 +1,14 @@
 package ru.kata.spring.boot_security.demo.model;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -12,104 +17,109 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
-import java.util.Collection;
+//import javax.persistence.Transient;
+//import java.util.Collection;
 import java.util.Set;
+import lombok.Data;
 
 @Entity
-@Table(name = "user")
-public class User implements UserDetails {
+@Data
+//геттеры, сеттеры, хэшкод и equals созданы ломбок
+@Table(name = "users")
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "firstname")
+    @Column(name = "username", unique = true)
+    @NotEmpty(message = "Имя не может быть пустым!")
     private String firstname;
-    @Column(name = "lastname")
     private String lastname;
-    @Column(name = "email")
-    private String email;
-    @Size(min=3, message = "Не меньше 3 знаков")
+    @Email    private String email;
+    @Size(min=3, max = 99, message = "Имя пользователя не менее 3 и не более 99 знаков")
     private String username;
-    @Size(min=3, message = "Не меньше 3 знаков")
+    @Size(min=8, message = "Пароль не менее 8 знаков")
     private String password;
-    @Transient
-    private String passwordConfirm;
+
     @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "users_roles",
+    joinColumns = @JoinColumn(name="user_id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
+    // getPassword и getUsername в ломбок
 
     public User() {
     }
 
-    public User(String firstname, String lastname, String email, String username, String password) {
+    public User(String username, String password, String firstname, String lastname, String email) {
+        this.username = username;
+        this.password = password;
         this.firstname = firstname;
         this.lastname = lastname;
         this.email = email;
-        this.username = username;
-        this.password = password;
     }
 
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    @Override
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return getRoles();
-    }
-    @Override
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public String getPasswordConfirm() {
-        return passwordConfirm;
-    }
-    public void setPasswordConfirm(String passwordConfirm) {
-        this.passwordConfirm = passwordConfirm;
-    }
-    public Set<Role> getRoles() {
-        return roles;
-    }
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
-    public String getFirstname() {        return firstname;    }
-    public void setFirstname(String firstname) {        this.firstname = firstname;    }
-    public String getLastname() {        return lastname;    }
-    public void setLastname(String lastname) {        this.lastname = lastname;    }
-    public String getEmail() {        return email;    }
-    public void setEmail(String email) {        this.email = email;    }
-    @Override
-    public String toString() {
-        return "Пользователь [" +
-                "id=" + id +
-                ", Логин = " + username + "]";
-    }
+//    public Long getId() {
+//        return id;
+//    }
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//    @Override
+//    public String getUsername() {
+//        return username;
+//    }
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return true;
+//    }
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return true;
+//    }
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return true;
+//    }
+//    @Override
+//    public boolean isEnabled() {
+//        return true;
+//    }
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return getRoles();
+//    }
+//    @Override
+//    public String getPassword() {
+//        return password;
+//    }
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//    public String getPasswordConfirm() {
+//        return passwordConfirm;
+//    }
+//    public void setPasswordConfirm(String passwordConfirm) {
+//        this.passwordConfirm = passwordConfirm;
+//    }
+//    public Set<Role> getRoles() {
+//        return roles;
+//    }
+//    public void setRoles(Set<Role> roles) {
+//        this.roles = roles;
+//    }
+//    public String getFirstname() {        return firstname;    }
+//    public void setFirstname(String firstname) {        this.firstname = firstname;    }
+//    public String getLastname() {        return lastname;    }
+//    public void setLastname(String lastname) {        this.lastname = lastname;    }
+//    public String getEmail() {        return email;    }
+//    public void setEmail(String email) {        this.email = email;    }
+//    @Override
+//    public String toString() {
+//        return "Пользователь [" +
+//                "id=" + id +
+//                ", Логин = " + username + "]";
+//    }
 }
