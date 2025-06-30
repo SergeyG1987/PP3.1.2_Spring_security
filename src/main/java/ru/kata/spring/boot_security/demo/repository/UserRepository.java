f36871277934b951a4417472a75bf8c76c99650a
Sergey <iamrussia@mail.ru>
2025-06-30 21:41:42 +0700

    добавление Optional в метод findByUsername в репозиторий UserRepository

--- src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
@@ -2,7 +2,10 @@ package ru.kata.spring.boot_security.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.kata.spring.boot_security.demo.entities.User;
 
+import java.util.Optional;
+
 //для выполнения стандартных запросов к БД
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+    //@Override
+    Optional<User> findByUsername(String username);
 }
