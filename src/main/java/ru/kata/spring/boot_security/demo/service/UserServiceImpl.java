3722c7fbc3e4bf4c8e3fb6617d8a45cec7b260a2
Sergey <iamrussia@mail.ru>
2025-07-01 21:36:03 +0700

    устранение замечаний в методе editUser

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -47,25 +47,44 @@ public class UserServiceImpl implements UserService {
         userRepository.deleteById(id);
     }
 
+//    @Transactional
+//    @Override
+//    public void editUser(Long id, User user) {
+//        User existingUser = userRepository.findById(id).orElse(null);
+//        if (existingUser != null) {
+//            existingUser.setUsername(user.getUsername());
+//            existingUser.setFirstName(user.getFirstName());
+//            existingUser.setLastName(user.getLastName());
+//            existingUser.setEmail(user.getEmail());
+//            existingUser.setRoles(user.getRoles());
+//            if (!existingUser.getPassword().equals(user.getPassword())) {
+//                existingUser.setPassword(
+//                        bCryptPasswordEncoder.encode(user.getPassword()));
+//            }
+//        } else {
+//            throw new UsernameNotFoundException(
+//                    String.format("User with id: %s not found", id));
+//        }
+//    }
     @Transactional
     @Override
     public void editUser(Long id, User user) {
-        User existingUser = userRepository.findById(id).orElse(null);
-        if (existingUser != null) {
-            existingUser.setUsername(user.getUsername());
-            existingUser.setFirstName(user.getFirstName());
-            existingUser.setLastName(user.getLastName());
-            existingUser.setEmail(user.getEmail());
-            existingUser.setRoles(user.getRoles());
-            if (!existingUser.getPassword().equals(user.getPassword())) {
-                existingUser.setPassword(
-                        bCryptPasswordEncoder.encode(user.getPassword()));
-            }
-        } else {
-            throw new UsernameNotFoundException(
-                    String.format("User with id: %s not found", id));
+        User existingUser = userRepository.findById(id)
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        String.format("User with id: %s not found", id)
+                ));
+        existingUser.setUsername(user.getUsername());
+        existingUser.setFirstName(user.getFirstName());
+        existingUser.setLastName(user.getLastName());
+        existingUser.setEmail(user.getEmail());
+        existingUser.setRoles(user.getRoles());
+        if (!existingUser.getPassword().equals(user.getPassword())) {
+            existingUser.setPassword(
+                    bCryptPasswordEncoder.encode(user.getPassword()));
         }
+        userRepository.save(existingUser);
     }
+
 
     @Override
     public User getUserById(Long id) {
